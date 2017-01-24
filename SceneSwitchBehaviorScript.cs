aae478fb438eae861c0098c6348bce3b0c09fefa
Larisa <furfreemi@yahoo.com>
2017-01-23 23:53:45 -0500

    initial behavior scripts for unity/hololens
    working copies

---
+++ SceneSwitchBehaviorScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchBehaviorScript : MonoBehaviour
+{
+    string nextScene = "school";
+    GestureRecognizer recognizer;
+    public static SceneSwitchBehaviorScript Instance { get; private set; }
+
+    void Start()
+    {
+        Instance = this;
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += (source, tapCount, ray) =>
+        {
+            SceneManager.LoadScene(nextScene);
+        };
+
+        recognizer.StartCapturingGestures();
+    }
+
+    void Update()
+    {
+
+    }
+}
